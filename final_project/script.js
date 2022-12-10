82c72602cadad283003bb74420a7f19561d0fec4
naterattner <naterattner@gmail.com>
2022-12-09 19:23:58 -0500

    another deploy attempt

--- final_project/script.js
+++ final_project/script.js
@@ -3,10 +3,9 @@ let myMap;
 myMap = L.map("map");
 
 // create tile layer
-L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png', {
-	maxZoom: 21,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="http://www.openstreetmap.bzh/" target="_blank">Breton OpenStreetMap Team</a>',
-	
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 myMap.setView([40.7080962, -73.9717345], 13);
